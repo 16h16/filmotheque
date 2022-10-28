ae0bddebaee361ff343d8c98650c5e82571c2171
Hamza Echamlali <echamlali.hamza@gmail.com>
2022-10-28 12:48:21 +0200

    Added columns in actors
    Added lastname, firstname, birth_date, gender

--- database/migrations/2022_10_28_103602_create_actors_table.php
+++ database/migrations/2022_10_28_103602_create_actors_table.php
@@ -11,11 +11,16 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
+        //Actors : id, lastname, firstname, birth_date, sex ;
         Schema::create('actors', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('lastname');
+            $table->string('firstname');
+            $table->date('birth_date');
+            $table->enum('gender', array('man', 'woman', 'not specify'));
         });
     }
 
@@ -24,7 +29,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('actors');
     }
