330a0a55e61891058abdd6877eb772dd907dc4f3
Hamza Echamlali <echamlali.hamza@gmail.com>
2022-10-28 12:27:32 +0200

    Relation between Category and Movie
    One category can have multiple movies

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Movie;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     use HasFactory;
+
+    public function movies(){
+        return $this->hasMany(Movie::class);
+    }
 }
